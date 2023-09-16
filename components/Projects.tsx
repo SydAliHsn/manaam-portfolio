3880ccf4619e5844ab59f165b3e8cb5f72f6de2f
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-09-16 16:11:16 +0500

    Create ProjectCard component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,63 +10,33 @@ const Projects = (props: {}): JSX.Element => {
         </h2>
         <p className='text-white text-lg sm:text-xl font-thin font-syne mt-4'>Some stuff I&apos;ve made trying to put my dent in the universe!</p>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
-            <div className="p-4" data-aos="flip-right" data-aos-duration="1000">
-                <img src="/project-1.png" alt="Image" className="max-h-[20rem]" />
-                <div className="mt-4">
-                    <h2 className="text-lg font-bold font-syne text-white">
-                        Brand Journey Improvements
-                    </h2>
-                    <div className="mt-2">
-                        <p className="text-sm text-gray-500 font-rubik">
-                            I made something really cool in this project using really cool technologies with my cool team.
 
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="p-4" data-aos="flip-left" data-aos-duration="1000">
-                <img src="/project-2.png" alt="Image" className="max-h-[20rem]" />
-                <div className="mt-4">
-                    <h2 className="text-lg font-bold font-syne text-white">
-                        Brand Grouping
-                    </h2>
-                    <div className="mt-2">
-                        <p className="text-sm text-gray-500 font-rubik">
-                            I made something really cool in this project using really cool technologies with my cool team.
 
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="p-4" data-aos="flip-right" data-aos-duration="1000">
-                <img src="/project-3.png" alt="Image" className="max-h-[20rem]" />
-                <div className="mt-4">
-                    <h2 className="text-lg font-bold font-syne text-white">NFT Glimps</h2>
 
-                    <div className="mt-2">
-                        <p className="text-sm text-gray-500 font-rubik">
-                            I made something really cool in this project using really cool technologies with my cool team.
-
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="p-4" data-aos="flip-left" data-aos-duration="1000">
-                <img src="/project-4.png" alt="Image" className="max-h-[20rem]" />
-                <div className="mt-4">
-                    <h2 className="text-lg font-bold font-syne text-white">
-                        Brand Suggestions
-                    </h2>
-                    <div className="mt-2">
-                        <p className="text-sm text-gray-500 font-rubik">
-                            I made something really cool in this project using really cool technologies with my cool team.
-
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <ProjectCard img='/project-4.png' description="I made something really cool in this project using really cool technologies with my cool team." title='Brand Suggestions' />
+            <ProjectCard img='/project-4.png' description="I made something really cool in this project using really cool technologies with my cool team." title='Brand Suggestions' />
+            <ProjectCard img='/project-4.png' description="I made something really cool in this project using really cool technologies with my cool team." title='Brand Suggestions' />
+            <ProjectCard img='/project-4.png' description="I made something really cool in this project using really cool technologies with my cool team." title='Brand Suggestions' />
+            <ProjectCard img='/project-4.png' description="I made something really cool in this project using really cool technologies with my cool team." title='Brand Suggestions' />
+            <ProjectCard img='/project-4.png' description="I made something really cool in this project using really cool technologies with my cool team." title='Brand Suggestions' />
         </div>
     </section>
 };
+
+const ProjectCard = ({ img, description, title }: { img: string, description: string, title: string }): JSX.Element => {
+    return <div className="p-4" data-aos="flip-left" data-aos-duration="1000">
+        <img src={img} alt="Image" className="max-h-[20rem] mx-auto sm:mx-0" />
+        <div className="mt-4">
+            <h2 className="text-lg font-bold font-syne text-white">
+                {title}
+            </h2>
+            <div className="mt-2">
+                <p className="text-sm text-gray-500 font-rubik">
+                    {description}
+                </p>
+            </div>
+        </div>
+    </div>
+}
 
 export default Projects;
