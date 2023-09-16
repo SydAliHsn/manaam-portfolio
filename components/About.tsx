55f8891a6b1621363a4ca3af9d4c3e0f02166c92
Syed Ali Hassan <syedalihassan6651@gmail.com>
2023-09-16 17:49:26 +0500

    Fix grammatical mistake

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ const About = (props: {}): JSX.Element => {
                 Let&apos;s get to know about me
             </h2>
             <p className="text-zinc-500 font-rubik font-thin w-full mt-6">
-                Hello! I am Embedded Software Developer and a Professional PCB designer with more than a year of experience in my field. During my embedded systems career, I have designed several systems based on IoT, gateway systems, BLE mesh network indeginous robots. I have also worked on designing Printed Circuit Boards(PCB) which mostly include battery charging circuits, battery Management systems, development boards and many others!
+                Hello! I am an Embedded Software Developer and a Professional PCB designer with more than a year of experience in my field. During my embedded systems career, I have designed several systems based on IoT, gateway systems, BLE mesh network indeginous robots. I have also worked on designing Printed Circuit Boards(PCB) which mostly include battery charging circuits, battery Management systems, development boards and many others!
             </p>
             <p className="text-zinc-500 font-rubik font-thin w-full mt-6">
                 My software career revolves around programming different micro-controllers like ESP32 , nRF series , AtMega and many more for various applications. I also have experience in working with different communication protocols like RS485, UART, JTAG etc along with wokring on different messaging protocols like MQTT, BLE Mesh etc.
